4e531d519d7a43baf55d5db5d08715a6195f8db5
kiran dwivedi <kirandwivedi530gmail.com>
2023-05-03 02:54:51 +0000

    layout builder

---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+// Write your code here
+import './index.css'
+
+import Footer from '../Footer'
+import Header from '../Header'
+import Body from '../Body'
+
+const Layout = () => (
+  <div>
+    <Header />
+    <Body />
+    <Footer />
+  </div>
+)
+
+export default Layout
